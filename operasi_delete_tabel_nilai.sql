a2b0e559ff15de7991d7198556ca185be8f46472
Banawi <masmuslim20151231@gmail.com>
2025-10-06 21:05:15 +0700

    Operasi update(Ubah jurusan Budi dari IPS ke IPA) dan delete(Hapus nilai Matematika milik Andi)

---
+++ operasi_delete_tabel_nilai.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.nilai (mata_pelajaran,nilai) VALUES
+	 ('Bahasa Inggris',90),
+	 ('PPKn',70),
+	 ('Sosial',90),
+	 ('IPA',90);
---
+++ operasi_update_tabel_siswa.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.siswa (nama,umur,jurusan) VALUES
+	 ('Andi',16,'IPA'),
+	 ('Awii',17,'IPA'),
+	 ('Anda',16,'IPS'),
+	 ('Dina',17,'IPA'),
+	 ('Budi',17,'IPA');
